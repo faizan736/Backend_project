722f5681b2a88c7c460d427fcaab413f17e472c4
faizan736 <shaik.basha1@learner.manipal.edu>
2025-03-10 23:24:48 +0530

    added comments to multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,8 @@
 import multer from "multer";
+// Multer, a middleware used in Node.js for handling file uploads.
 
+
+//multer.diskStorage() defines how and where the uploaded files should be stored.
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './public/temp')
@@ -11,3 +14,16 @@ const storage = multer.diskStorage({
   })
   
  export  const upload = multer({ storage: storage })
+
+ /*
+
+ a. destination Function:
+
+Determines the folder where files will be saved.
+In this case, files will be stored in the ./public/temp folder.
+cb is a callback function that tells Multer where to save the file. The null means no error occurred.
+b. filename Function:
+
+Determines the name of the saved file.
+file.originalname ensures the file keeps its original name
+ */
